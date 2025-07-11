beb8e3c8ba1cd3ae777b489e1ed3823d98431f61
Bryan King <bpking15@gmail.com>
2025-07-11 12:26:22 -0400

    Refactor app.vue to use NuxtLayout and add index.vue for the homepage. The index.vue file introduces a welcome message and project description for the Windtunnel interface prototyping environment.

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the Windtunnel</h1>
+    <p>This is Navistone's interface prototyping environment. It's a project bootstrapped with Nuxt, the same framework that powers the Navistone platform UI.</p>
+  </div>
+</template>
